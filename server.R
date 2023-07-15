7e3f9cc9e3dba5699c27416ec718ed62f7e227d5
rosericazondekon <roseric_2000@yahoo.fr>
2023-07-14 22:59:03 -0400

    Reorganize scripts and files

--- server.R
+++ server.R
@@ -1,139 +1,11 @@
-library(shiny)
-library(shinyWidgets)
-library(shinymeta)
-library(DT)
-library(tidyverse)
-library(randomForest)
-library(rmarkdown)
-library(Hmisc)
-library(sf)
-library(rgdal)
-library(rmapshaper)
-library(tmap)
-library(leaflet)
-library(rmapshaper)
-library(maps)
-library(wesanderson)
-library(knitr)
-library(kableExtra)
-library(tmaptools)
-library(gtsummary)
+#------------------------------------------------------------------------------------
+# malDecision App
+# Original idea: Savi Koissi
+# Edit and Enhancement: Gbedegnon Roseric Azondekon
+#
+# Atlanta, July 2023
+#------------------------------------------------------------------------------------
 
-
-options(shiny.maxRequestSize = 100 * 1024 ^ 2)
-
-data(World)
-
-# UI
-ui <- fluidPage(
-  
-  tagList(
-    # CSS for navbar elements
-    tags$style(
-      HTML('.navbar {font-size: 17px;}',
-           '.navbar-default .navbar-brand {color: #262626; font-weight: bold}',
-           '.navbar-default .navbar-brand:hover {color: #262626;}',
-           '.navbar-default .navbar-nav > li > a {color: #262626;}')
-    ), 
-    # CSS for fonts
-    tags$style('h3 {font-weight: normal;}',
-               'h4 {font-weight: normal;}',
-               '* {font-family: Ubuntu;}'),
-    # CSS for all buttons (class .btn)
-    tags$style('.btn {color: #333333; 
-                      background-color: #eeeeee; 
-                      border-color: #cccccc;}
-                .btn:hover {background-color: #e1e1e1;}'), 
-    # CSS for errors and validation messages
-    tags$style('.shiny-output-error-validation {
-                 color: #e35300;
-                 font-weight: bold;}'),
-    # CSS for individual elements: note the '#id' syntax
-    tags$style('#map-readme {color: #333333;
-                             background-color: #ff770050;
-                             border-color: #c84407;}
-                #map-readme:hover {background-color: #e36a0075;}
-                #trend-readme {color: #333333;
-                               background-color: #ff770050;
-                               border-color: #c84407;}
-                #trend-readme:hover {background-color: #e36a0075;}')
-  ),
-  navbarPage(
-    title = "", 
-    theme = shinythemes::shinytheme("readable"),
-  )
-  ,
-  
-  
-  titlePanel(
-    title = div(
-      img(
-        src = "FullLogo_Transparent_NoBuffer.png",
-        width = "30%",
-        height = "20%",
-        style = "float: left:"
-      ),
-      "malDecision: Interactive Tool For Informed-Decision-Making"
-    )
-  ),
-  
-  downloadButton("download", "Download.csv"),
-  downloadButton("download_report", "Download Report"),
-  
-  align = "center",
-  sidebarLayout(
-    sidebarPanel(
-      id = "sidebarPanel",
-      selectInput("country", "Select a country:", choices = unique(World$name)),
-      fileInput("epi_file", "Upload Epi Data", accept = c(".csv", ".txt", ".xlsx", ".xls")),
-      
-      selectInput("latitude_column", "Select Latitude (x)", choices = NULL),
-      selectInput("longitude_column", "Select Longitude (y)", choices = NULL),
-      selectInput("cluster_var", "Select Degree of urbanization (Cluster Variable)", choices = NULL),
-      selectInput(
-        "classifier",
-        "Select Determinant(s) (Classifier Variable(s))",
-        choices = NULL,
-        multiple = TRUE
-      ),
-      selectInput("dependent_var", "Select Dependent Variable (Prevalence /Incidence)", choices = NULL),
-      actionButton("run_analysis", "Run Analysis")
-    ),
-    mainPanel(tabsetPanel(
-      # tabPanel("Map", leafletOutput("map"), 
-               # helpText("Please check urbanization map after running the analysis!")), 
-      tabPanel("Urbanization map", leafletOutput("map2"), 
-               helpText("Please check variable importance on the next tab!")),
-      tabPanel(
-        "Variance Importance",
-        dataTableOutput("var_imp_table"),
-        plotOutput("var_imp_plot"), 
-        helpText("Please check summary statistics on the next tab!")
-      ),
-      tabPanel("Report", dataTableOutput("report"),
-              helpText("Please go back!!!"))
-    ))
-  ),
-  
-  
-  tags$head(tags$script(
-    HTML(
-      "
-        <!-- Google tag (gtag.js) -->
-        <script async src='https://www.googletagmanager.com/gtag/js?id=G-VGTSCPCLQQ'></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-VGTSCPCLQQ');
-        </script>
-      "
-    )
-  ))
-)
-
-# Server
 server <- function(input, output, session) {
   #selected country
   selected_country <- reactive(World[World$name == input$country, ])
@@ -148,8 +20,8 @@ server <- function(input, output, session) {
     map <- tm_shape(selected_country()) +
       tm_borders(lwd = 1, col = "skyblue") +
       tm_fill(alpha = 0.5, col = "skyblue")
-     map
-     #tmap_leaflet(map, in.shiny = TRUE)
+    map
+    #tmap_leaflet(map, in.shiny = TRUE)
   })
   
   get_point <- reactive({ 
@@ -162,7 +34,7 @@ server <- function(input, output, session) {
   
   get_map <- reactive({
     req(selected_country(),  get_point())
-
+    
     tm_shape(selected_country()) +
       tm_borders()+
       tm_shape(get_point()) +
@@ -171,11 +43,11 @@ server <- function(input, output, session) {
     # map_point #tmap_leaflet(map_point, in.shiny = TRUE)
   })
   
-    output$map2 <- renderTmap({
-          get_country_map()
-      })
-    
-
+  output$map2 <- renderTmap({
+    get_country_map()
+  })
+  
+  
   
   # Keep a reference to the projected points
   projected_points <- reactiveVal(NULL)
@@ -255,10 +127,10 @@ server <- function(input, output, session) {
     # selected_data <- epi_data()[, selected_cols]
     # 
     # s_data <- lapply(split(selected_data, epi_data()$cluster_var), summary)
-      
+    
     summarise_data <- epi_data() %>% 
       summary()
-      #do.call(rbind, s_data)
+    #do.call(rbind, s_data)
     
     
     cluster_data <- epi_data() %>%
@@ -339,10 +211,10 @@ server <- function(input, output, session) {
   output$report <- renderDataTable({
     req(analysis_results())
     # analysis_results()$summarise_data
-     datatable(analysis_results()$summarise_data)
-
+    datatable(analysis_results()$summarise_data)
+    
   })
-
+  
   
   # Render the leaflet map
   # output$map <- renderTmap({
@@ -393,5 +265,3 @@ server <- function(input, output, session) {
   )
   
 }
-
-shinyApp(ui = ui, server = server)
