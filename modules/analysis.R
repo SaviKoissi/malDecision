1e89ca2463b8dd561e8b24b5504582dc01aeae40
rosericazondekon <roseric_2000@yahoo.fr>
2023-07-17 15:31:57 -0400

    Improve analysis module

--- modules/analysis.R
+++ modules/analysis.R
@@ -1,22 +1,22 @@
 analysisModule <- function(input, output, session, configs){
+  
+  summarise_data <- reactive({
+    configs$epi_data() %>% summary()
+  })
+  
   # Run analysis and generate outputs
-  analysis_results <- reactive({
+  var_importance_df <- reactive({
     req(
       configs$run_analysis(),
       configs$epi_data(),
-      configs$cluster_var,
-      configs$classifier,
-      configs$dependent_var
+      configs$cluster(),
+      configs$classifier(),
+      configs$dependent()
     )
     
-    cluster_var <- configs$cluster_var()
+    cluster_var <- configs$cluster()
     classifiers <- configs$classifier()
     dependent_var <- configs$dependent()
-    
-    summarise_data <- configs$epi_data() %>% 
-      summary()
-    #do.call(rbind, s_data)
-    
     
     cluster_data <- configs$epi_data() %>%
       dplyr::select(all_of(cluster_var))
@@ -53,28 +53,31 @@ analysisModule <- function(input, output, session, configs){
         arrange(Importance)
     })
     
-    var_importance_df <- do.call(rbind, var_importance)
-    
-    # Plot of Variable Importance for each cluster
-    var_imp_plot <- ggplot(
-      var_importance_df,
-      aes(
-        x = reorder(Variable, Importance),
-        y = Importance,
-        fill = factor(Cluster)
-      )
-    ) +
+    importance_df <- do.call(rbind, var_importance)
+    importance_df
+  })
+  
+  # Plot of Variable Importance for each cluster
+  var_imp_plot <- reactive({
+    var_importance_df() %>% 
+      ggplot(
+        aes(
+          x = reorder(Variable, Importance),
+          y = Importance,
+          fill = factor(Cluster)
+        )
+      ) +
       geom_bar(stat = "identity") +
       labs(x = "Variable", y = "Importance", title = "Variable Importance") +
       theme_minimal() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
       scale_fill_manual(values = wes_palette("Royal1")) + #low = "blue", high = "darkblue" wes_palette("FantasticFox1")
       labs(caption = paste("malDecision", format(Sys.Date(), "%Y")))
-    
-    list(
-      var_importance = var_importance_df,
-      var_imp_plot = var_imp_plot,
-      summarise_data = summarise_data
-    )
   })
+  
+  list(
+    var_importance = var_importance_df,
+    var_imp_plot = var_imp_plot,
+    summarise_data = summarise_data
+  )
 }
