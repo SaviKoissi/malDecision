eab00f7d7bacf84b8bfc76b0b208bf92ce4bc2d7
rosericazondekon <roseric_2000@yahoo.fr>
2023-07-17 13:30:39 -0400

    Move old ui and server scripts

---
+++ old/global.R
@@ -0,0 +1,29 @@
+#------------------------------------------------------------------------------------
+# malDecision App
+# Original idea: Savi Koissi
+# Edit and Enhancement: Gbedegnon Roseric Azondekon
+#
+# Atlanta, July 2023
+#------------------------------------------------------------------------------------
+
+options(shiny.maxRequestSize = 100 * 1024^2)
+
+suppressPackageStartupMessages({
+  libs <- c(
+    "shiny", "shinyWidgets", "shinymeta", "DT", "tidyverse", "rmarkdown", 
+    "Hmisc", "sf", "rmapshaper", "maps", "knitr", "kableExtra", "tmap",
+    "tmaptools", "gtsummary", "shinycssloaders", "leaflet"
+  )
+  
+  if (length(setdiff(libs, rownames(installed.packages()))) > 0) {
+    install.packages(setdiff(libs, rownames(installed.packages()))
+                     , repos = "http://cran.us.r-project.org")
+  }
+  
+  lapply(libs, library, character.only = TRUE, quietly = TRUE)
+})
+
+
+options(shiny.maxRequestSize = 100 * 1024 ^ 2)
+
+data(World)
