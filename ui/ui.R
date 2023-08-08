913f36691aef61cc367609da97d6e59685601114
rosericazondekon <roseric_2000@yahoo.fr>
2023-08-08 00:06:00 -0400

    Reorganize app ui

--- ui/ui.R
+++ ui/ui.R
@@ -7,78 +7,25 @@
 #------------------------------------------------------------------------------------
 
 appUI <- function() {
-  fluidPage(
-    
-    tagList(
-      # CSS for navbar elements
-      tags$style(
-        HTML('.navbar {font-size: 17px;}',
-             '.navbar-default .navbar-brand {color: #262626; font-weight: bold}',
-             '.navbar-default .navbar-brand:hover {color: #262626;}',
-             '.navbar-default .navbar-nav > li > a {color: #262626;}')
-      ), 
-      # CSS for fonts
-      tags$style('h3 {font-weight: normal;}',
-                 'h4 {font-weight: normal;}',
-                 '* {font-family: Ubuntu;}'),
-      # CSS for all buttons (class .btn)
-      tags$style('.btn {color: #333333; 
-                      background-color: #eeeeee; 
-                      border-color: #cccccc;}
-                .btn:hover {background-color: #e1e1e1;}'), 
-      # CSS for errors and validation messages
-      tags$style('.shiny-output-error-validation {
-                 color: #e35300;
-                 font-weight: bold;}'),
-      # CSS for individual elements: note the '#id' syntax
-      tags$style('#map-readme {color: #333333;
-                             background-color: #ff770050;
-                             border-color: #c84407;}
-                #map-readme:hover {background-color: #e36a0075;}
-                #trend-readme {color: #333333;
-                               background-color: #ff770050;
-                               border-color: #c84407;}
-                #trend-readme:hover {background-color: #e36a0075;}')
-    ),
-    navbarPage(
-      title = "", 
-      theme = shinythemes::shinytheme("readable"),
-    ),
-    
-    
-    titlePanel(
-      title = div(
-        img(
-          src = "FullLogo_Transparent_NoBuffer.png",
-          width = "30%",
-          height = "20%",
-          style = "float: left:"
+    # dependent_pkgs <- readRDS(file = "gvsa/data/required-packages.rds")
+    # dependent_pkgs <- setdiff(dependent_pkgs, c("tools", "parallel", "shiny"))
+    list(
+      shinyjs::useShinyjs(),
+      tags$head(tags$link(rel = "stylesheet", href = "style.css")),
+      navbarPage(
+        title = a(
+          href = "https://github.com/SaviKoissi/malDecision",
+          target = "_blank",
+          tags$img(src = "logo-white.png", id="logo")
         ),
-        "malDecision: Interactive Tool For Informed-Decision-Making"
+        id = "mainNavBar",
+        windowTitle = "malDecision: Data Driven Malaria Decision Making",
+        collapsible = TRUE,
+        inverse = TRUE,
+        position = "fixed-top",
+        home,
+        analyzer,
+        about
       )
-    ),
-    
-    downloadButton("download", "Export Importance Table"),
-    reportModuleOutput("report"),
-    
-    align = "center",
-    layoutUI,
-    
-    
-    tags$head(tags$script(
-      HTML(
-        "
-        <!-- Google tag (gtag.js) -->
-        <script async src='https://www.googletagmanager.com/gtag/js?id=G-VGTSCPCLQQ'></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-VGTSCPCLQQ');
-        </script>
-      "
-      )
-    ))
-  )
+    )
 }
