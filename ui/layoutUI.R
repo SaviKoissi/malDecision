2a1ad21bb8112af4cb099dccfe34e044846d2f2e
rosericazondekon <roseric_2000@yahoo.fr>
2023-08-08 00:05:47 -0400

    Update app layout

--- ui/layoutUI.R
+++ ui/layoutUI.R
@@ -6,7 +6,52 @@
 # Atlanta, July 2023
 #------------------------------------------------------------------------------------
 
-layoutUI <- sidebarLayout(
-  sideBarModuleOutput("sideBar"),
-  mainPanelModuleOutput("mainPanel")
+home <- tabPanel(
+  "Home",
+  shiny::includeHTML("static/home.html"),
+  div(
+    class = "row",
+    div(
+      class = "col-md-6 col-md-offset-3 text-center",
+      hr(class="my-4"),
+      actionButton(
+        "gotoapp",
+        class = "btn btn-success",
+        label = "Go to the malDecision Analyzer")
+    )
+  ),
+  p("")
 )
+
+analyzer <- tabPanel(
+  "Analyzer",
+  value = "analyzer",
+  sidebarLayout(
+    sideBarModuleOutput("sideBar"),
+    mainPanelModuleOutput("mainPanel")
+  )
+)
+
+faq <- tabPanel(
+  "FAQs and Quick Guide",
+  shiny::includeHTML("static/faq.html")
+)
+
+license <- tabPanel(
+  "License",
+  shiny::includeHTML("static/license.html")
+)
+
+feedback <- tabPanel(
+  "Feedback",
+  shiny::includeHTML("static/feedback.html")
+)
+
+
+about <- navbarMenu(
+  "About",
+  faq,
+  license,
+  feedback
+)
+
